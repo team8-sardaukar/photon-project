b591aafe20477e923e48f94a677f3703833a415b
Arenile <theivris@gmail.com>
2021-10-04 13:48:18 -0500

    The application now redirects from the landing page (future splash screen) to the page for player entry after 3 seconds.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PlayersDBService } from './players/players-DB.service';
 import { Player } from './players/player.model';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+/* This component implements the splash screen, and also
+ * is the background that the rest of the pages
+ * will be rendered on top of.
+ */
+
 
 /*  The stuff commented out in this file will prevent
  *  the front end from compiling because it can't pull
@@ -13,27 +20,23 @@ import { Subscription } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent /*implements OnInit, OnDestroy*/ {
+export class AppComponent implements OnInit/*, OnDestroy*/ {
   title = 'frontend';
   playersListSubs = Subscription;
   //playersList: Player[];
 
-  constructor (private playersDB: PlayersDBService)
+  constructor (private playersDB: PlayersDBService, 
+    private router: Router)
   {
 
   }
 
   ngOnInit() 
   {
-  /*  this.playersListSubs = this.playersDB
-    
-    .getPlayers()
-    .subscribe(res => 
-      {
-        this.playersList = res;
-      },
-      console.error);
-      */
+    //Redirect to player entry screen after 3 seconds on splash
+    setTimeout(() => {
+      this.router.navigate(['/player-entry']);
+    }, 3000);
   }
 
   ngOnDestroy() 
