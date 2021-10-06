43bce4782a7e583541317b1f85300027942a2eab
Arenile <theivris@gmail.com>
2021-10-06 14:21:41 -0500

    Preparing filesystem for Heroku deployment

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from flask import Flask, request, render_template, redirect
 from flask.helpers import send_from_directory
 from flask_cors import CORS
-from urllib.parse import urlparse
 import os
 
-app = Flask(__name__)
-if __name__ == '__main__':
-    app.run()
-    CORS(app)
+#template_dir = os.path.abspath('/dist')
+#absTest = '/home/ivris/Documents/photon-project/templates'
+app = Flask(__name__
+#, template_folder=absTest
+)
 
 @app.route('/players')
 def get_player_by_id():
     print('Does nothing')
     # This is where we will interact with the database
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    o = urlparse(request.base_url)
-    send = o.hostname + ':8080'
-    return redirect(send)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+    CORS(app)
