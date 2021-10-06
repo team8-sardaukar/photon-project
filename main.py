f2ab0b5e5742d48172f62a479ffccbabe949c4cd
Arenile <theivris@gmail.com>
2021-10-06 03:48:51 -0500

    Attempt to see if flask will pass routing to angular

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/players')
-
 def get_player_by_id():
     print('Does nothing')
     # This is where we will interact with the database
+
+@app.route('/')
+def home():
+    return render_template('src/index.html')
