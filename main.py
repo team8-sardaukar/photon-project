79c6704670b6cd9239c8fd0db795d4395a0b96a5
Arenile <theivris@gmail.com>
2021-10-14 20:05:22 -0500

    You can now add players via the application, they go to the correct time, and you can also just enter the ID of known players to add to the game.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, render_template, jsonify, flash
+from sqlalchemy.sql.expression import update
 from flask_cors import CORS
 from entities.entity import Session, engine, Base
 from entities.player import Player
 
-#template_dir = os.path.abspath('/dist')
-#absTest = '/home/ivris/Documents/photon-project/templates'
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -12,16 +11,28 @@ Base.metadata.create_all(engine)
 session = Session()
 players = session.query(Player).all()
 
-playersList = [
+#Just an example of the player list dicts formatting
+playersListEx = [ 
     {
-        'id':'01221',
-        'codename':'TESTER'
-    },
-    {
-        'id':'01222',
-        'codename':'OTHER'
+        'id':'',
+        'codename':''
     }
 ]
+
+playersListGreen = []
+playersListRed = []
+
+def updatePlayers(id, codename, team=0): #team 0 = green, 1 = red
+    global players
+    global playersListRed
+    global playersListGreen
+    for player in players:
+        if (int(id) == player.id):
+            newPlayerDict = {"id":id, "codename":codename}
+            if (team == 0):
+                playersListGreen.append(newPlayerDict.copy())
+            else:
+                playersListRed.append(newPlayerDict.copy())  
 
 @app.route('/', methods=['GET'])
 def home():
@@ -33,51 +44,65 @@ def playerEntry():
 
 @app.route('/player-entry', methods=['GET'])
 def playerEntryScreen():
-    # Still needs to submit to database
     return render_template('player-entry.html')
 
 @app.route('/player-entry', methods=['POST'])
 def getPlayerByID():
+    global players
+    global playersListGreen
+    global playersListRed
     greenId = request.form["GreenId0"]
     greenName = request.form["GreenName0"]
-    greenEntry = [greenId, greenName]
     redId = request.form["RedID0"]
     redName = request.form["RedName0"]
-    redEntry = [redId, redName]
-    #global player
-    #return jsonify(player['data'])
     if redId and redName and greenId and greenName:
-        #submit the 
-        return jsonify(greenEntry, redEntry)
+        entry = Player(greenId, greenName)
+        session.add(entry)
+        session.commit()
+        entry = Player(redId, redName)
+        session.add(entry)
+        session.commit()
+        players = session.query(Player).all()
+        updatePlayers(redId, redName, 1)
+        updatePlayers(greenId, greenName, 0)
+        return render_template('player-entry.html', redList = playersListRed, greenList = playersListGreen)
     elif not redId and greenId:
         if not greenName:
             foundEntry = 0
             for player in players:
                 if (int(greenId) == player.id):
-                    newPlayerDict = {"id":greenId, "codename":player.codename}
-                    playersList.append(newPlayerDict.copy())
+                    updatePlayers(greenId, player.codename, 0)
                     foundEntry = 1
             if (foundEntry != 1):
                 flash('NO PLAYER FOUND BY THAT ID! ENTER A NEW CODENAME FOR THAT PLAYER ID!')
-            return render_template('player-entry.html', data=playersList)
+            return render_template('player-entry.html', greenList = playersListGreen, redList = playersListRed)
         else:
             entry = Player(greenId, greenName)
-            print(str(entry.id) + str(entry.codename))
             session.add(entry)
             session.commit()
-            return render_template('player-entry.html', data=playersList)
+            players = session.query(Player).all()
+            updatePlayers(greenId, greenName, 0)
+            return render_template('player-entry.html', greenList = playersListGreen, redList = playersListRed)
     elif not greenId and redId:
         if not redName:
-            return jsonify(redId)
+            foundEntry = 0
+            for player in players:
+                if (int(redId) == player.id):
+                    updatePlayers(redId, player.codename, 1)
+                    foundEntry = 1
+            if (foundEntry != 1):
+                flash('NO PLAYER FOUND BY THAT ID! ENTER A NEW CODENAME FOR THAT PLAYER ID!')
+            return render_template('player-entry.html', greenList = playersListGreen, redList = playersListRed)
         else:
-            return jsonify(redEntry)
+            entry = Player(redId, redName)
+            session.add(entry)
+            session.commit()
+            players = session.query(Player).all()
+            updatePlayers(redId, redName, 1)
+            return render_template('player-entry.html', greenList = playersListGreen, redList = playersListRed)
     else:
         return render_template('player-entry.html')
 
 if __name__ == '__main__':
     app.run()
     CORS(app)
-
-print('PLAYERS: ')
-for player in players:
-    print(f'({player.id}) {player.codename}')
